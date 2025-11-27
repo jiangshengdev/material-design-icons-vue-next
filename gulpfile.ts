1c6dce7211498febe3981d79978c0e747d732f0a
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-27 14:21:20 +0800

    move

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,7 @@
-import generateDemo from './build/tasks/generate-demo';
-import generateIcons from './build/tasks/generate-icons';
-import generateIndex from './build/tasks/generate-index';
-import { cleanSrc } from './build/tasks/clean-directories';
+import generateDemo from './scripts/tasks/generate-demo';
+import generateIcons from './scripts/tasks/generate-icons';
+import generateIndex from './scripts/tasks/generate-index';
+import { cleanSrc } from './scripts/tasks/clean-directories';
 import { parallel, series } from 'gulp';
 
 const icon = series(
